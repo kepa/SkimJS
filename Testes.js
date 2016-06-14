75bb00e8ec2859e481243a12fcada4d75865e645
Lucas Lucena Falbo <95.lucas@gmail.com>
2016-06-13 22:50:38 -0300

    Gerando testes

---
+++ Testes.js
@@ -0,0 +1,30 @@
+// Teste 1
+var x = 1, y = 10;
+if (x > 1) {
+	y;
+}
+
+//Teste 2
+if (y < 5) {
+	x = 4;
+} else {
+	y = x + y;
+}
+
+//Teste 3
+while(x < 3) {
+	x = x + 1;
+}
+
+//Teste 4
+while(y < 40) {
+	y = y + 1;
+	break;
+}
+
+//Teste 5
+lista = [2,4,6,8,10];
+lista[2];
+
+//Teste 6
+lista.length();
